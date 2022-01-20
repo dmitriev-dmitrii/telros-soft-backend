f1a74a6ca7d85cf6f377be95a51a1243779db092
dmitriev dmitrii <dmitriev.dmitrii@mail.com>
2022-01-20 14:10:40 +0300

    test 3

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
-
-import express from 'express';
-
-const app =  express();
+const express = require('express');
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
-app.get('/', function (req, res) {
-	res.send('home');
-});
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const cors = require('cors');
+app.use(cors());
+
+// const path = require( 'path' );
+// app.use(express.static(path.join(__dirname, 'result-data')));
 
 app.get('/users', function (req, res) {
-	res.send('users');
+	res.send('/users');
 });
 
 app.get('/users/:id', function(req , res){
 	res.send(req.params.id);
 });
 
+// app.post('/img-rocessing',async function  (req, res) {
+// 	if(!req.body) return response.sendStatus(400);
+// 	// console.log(req.body);
+// 	resultData = await 	imgProcessing( req );
+// 	// console.log( resultData );
 
+// 	// console.log( res.headers );
+	
+// 	res.send({resultData});
+// 	res.end();
+// });
 
-export default  app;
+module.exports = app;
